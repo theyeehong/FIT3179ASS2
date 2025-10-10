a4f0a5b6d69582c8f15ac399255928838cd02a0a
Horngy <mynameistehyeehong@icloud.com>
2025-10-10 18:53:51 +1100

    week10

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,7 @@ var vg1 = "js/map.vg.json";
 var vg2 = "js/gbar.vg.json";
 
 let vg1View = null;
+let vg2View = null;
 
 document.addEventListener('DOMContentLoaded', function() {
     function renderCharts(year) {
@@ -13,6 +14,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 })
         } else {
             vg1View.signal('YearSelection', year).runAsync();
+        }
+        if (!vg2View) {
+            vegaEmbed("#vg2", vg2, { actions: false })
+                .then(result => {
+                    vg2View = result.view;
+                    vg2View.signal('YearSelection', year).runAsync();
+                })
+        } else {
+            vg2View.signal('YearSelection', year).runAsync();
         }
     }
 
