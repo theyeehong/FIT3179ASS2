c97eebddadf8518c1fbc29926a2728c45e76ee54
Horngy <mynameistehyeehong@icloud.com>
2025-10-10 18:23:25 +1100

    new dataset

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,25 @@
 var vg1 = "js/map.vg.json";
+var vg2 = "js/gbar.vg.json";
 
-vegaEmbed("#vg1", vg1).then(function(result) {}).catch(console.error);
+let vg1View = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+    function renderCharts(year) {
+        if (!vg1View) {
+            vegaEmbed("#vg1", vg1, { actions: false })
+                .then(result => {
+                    vg1View = result.view;
+                    vg1View.signal('YearSelection', year).runAsync();
+                })
+        } else {
+            vg1View.signal('YearSelection', year).runAsync();
+        }
+    }
+
+    renderCharts(2014);
+
+    document.getElementById('year-select').addEventListener('change', function() {
+        const selectedYear = parseInt(this.value);
+        renderCharts(selectedYear);
+    });
+});
